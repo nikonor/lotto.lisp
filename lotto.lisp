23fb81a8253dd57be605c40a9bec48aceecbd9c6
Leonid Nikolaev <nikonor@nikonor.ru>
2012-05-12 14:16:37 +0400

    Первый коммит 	new file:   README.md 	new file:   lotto.lisp

---
+++ lotto.lisp
@@ -0,0 +1,18 @@
+(defun lotto(col max) 
+	(setf game (list))
+	
+	(loop for i from 1 to 100 while (< (list-length game) col) 
+		do 
+		(setf r (random max))
+		(if (= (list-length (member r game)) 0)
+			(if (> r 0)
+				(push r game)
+				; (format t "0=~S~%" r)
+			)
+			; (format t "r=~S~%" r)
+		)
+		; (push r game)
+	)
+	(sort game #'<)
+	game
+)
